9b67dd1695f287afc578387d4fbe45784621e79a
jerjjj <wuzhijun@jerjjj.cn>
2026-03-23 18:56:54 +0800

    更新了教务系统地址

--- native/src/constants.rs
+++ native/src/constants.rs
@@ -4,4 +4,4 @@ pub const VPN_MARKER: &str = "vpn-12-o2-jwxt.cwnu.edu.cn";
 
 /// WebVPN 教务代理根地址
 pub const JWXT_APP_BASE: &str = "https://webvpn.cwnu.edu.cn/https\
-     /77726476706e69737468656265737421fae0598869337f5e6b468ca88d1b203b";
+     /513935263772404239354537256e68376b74a107323f47a0837f7923ef8ac463";
